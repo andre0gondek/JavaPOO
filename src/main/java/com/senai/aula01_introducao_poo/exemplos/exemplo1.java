ea6cb9f281209c4f1b08f0fbd489393c19e008d3
andremendes <andregondek42@gmail.com>
2025-01-30 11:13:26 -0300

    classe main

--- src/main/java/com/senai/aula01_introducao_poo/exemplos/exemplo1.java
+++
@@ -1,5 +0,0 @@
-package com.senai.aula01_introducao_poo.exemplos;
-
-public class exemplo1 {
-
-}
---
+++ src/main/java/com/senai/aula01_introducao_poo/exemplos/pessoa/Main.java
@@ -0,0 +1,9 @@
+package com.senai.aula01_introducao_poo.exemplos.pessoa;
+
+public class Main {
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.nome = "André Mendes";
+        System.out.println(pessoa1.nome);
+    }
+}
