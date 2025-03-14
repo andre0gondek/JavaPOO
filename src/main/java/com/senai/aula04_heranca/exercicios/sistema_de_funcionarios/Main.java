0cbc0fb3f641f907d19f922fb0ccb1ae7b834ad2
andremendes <andregondek42@gmail.com>
2025-03-14 11:08:56 -0300

    exercicio 1 da aula 04 terminado, exercicio 2 pendente

--- src/main/java/com/senai/aula04_heranca/exercicios/sistema_de_funcionarios/Main.java
+++ src/main/java/com/senai/aula04_heranca/exercicios/sistema_de_funcionarios/Main.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Professor professor1 = new Professor("Thiago",6000.0,"Matemática");
-        Professor professor2 = new Professor("Marco Antônio", 6000.0,"História");
+        Professor professor2 = new Professor("Marco Antônieto", 6000.0,"História");
         Professor professor3 = new Professor("Rafael",6000.0,"LOPAlPython");
 
         ArrayList<Professor> professores = new ArrayList<>();
